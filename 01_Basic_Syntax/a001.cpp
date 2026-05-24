e7584c31bb57ada4a83c035b45089d385dafc0cc
Yu-En Li <275398805+yu-en-li@users.noreply.github.com>
2026-05-24 15:48:32 +0800

    feat: a001哈囉

---
+++ 01_Basic_Syntax/a001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    while (cin >> s) {
+        cout << "hello, " << s << endl;
+    }
+    return 0;
+}
