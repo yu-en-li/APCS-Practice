bceab16198bb8cd10e0b4001c31b5c83962bd5b5
Yu-En Li <275398805+yu-en-li@users.noreply.github.com>
2026-05-28 06:08:06 +0800

    feat: a034. 二進位制轉換

---
+++ 01_Basic_Syntax/03_Loops/a034_Binary_Conversion_String.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    while (cin >> n) {
+        // 特判 0 的狀況，處理完 continue 
+        if (n == 0) {
+            cout << "0\n";
+            continue; 
+        }
+
+        string ans = "";
+        while (n > 0) {
+            ans.push_back((n & 1) + '0');
+            n >>= 1;
+        }
+
+        reverse(ans.begin(), ans.end());
+        cout << ans << "\n";
+    }
+    return 0;
+}
