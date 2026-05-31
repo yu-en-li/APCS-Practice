353129858ab536544e845989b980c0e74f396632
Yu-En Li <275398805+yu-en-li@users.noreply.github.com>
2026-05-31 08:47:28 +0800

    feat(a034): add Python solution

--- 03_Basic_Algorithms/06_Math_Theory/a034_Binary_Conversion_String.py
+++ 03_Basic_Algorithms/06_Math_Theory/a034_Binary_Conversion_String.py
@@ -2,7 +2,7 @@
 # APCS Complexity: O(log N)
 # APCS Tag: 基礎語法, 數學與數論
 # APCS Difficulty: 2
-# APCS Note: 
+# APCS Note: https://www.notion.so/a034-36a43be958cd80a49057f8b8925ed00d?v=36a43be958cd8075b3ac000c2c628f5d&source=copy_link
 
 import sys
 while True:
